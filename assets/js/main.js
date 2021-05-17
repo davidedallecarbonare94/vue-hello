498b3bc862d25f9d5a39ff3ba2d3a06575e65c08
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-05-17 14:42:24 +0200

    end project

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* 
+Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+//Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+let hello = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Hi and welcome to VueJs"
+        }
+
+    }
+)
